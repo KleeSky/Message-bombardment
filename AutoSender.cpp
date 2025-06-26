29968521dad76117820004cae907a208a8e6fef3
KleeSky <kleesky@foxmail.com>
2025-06-26 15:35:46 +0800

    添加新功能

--- AutoSender.cpp
+++ AutoSender.cpp
@@ -6,6 +6,7 @@
 
 namespace AutoSender {
 
+    // 模拟键盘输入单个字符
     void sendChar(char ch) {
         SHORT vk = VkKeyScanA(ch);
         if (vk == -1) {
@@ -30,22 +31,41 @@ namespace AutoSender {
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
+	// 模拟按下回车键
     void sendEnter() {
         keybd_event(VK_RETURN, 0, 0, 0);
         keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
     }
 
     void sendMessages(const std::string& message, int repeatCount, double intervalBetweenMessagesMs, int intervalBetweenCharsMs) {
-        for (int i = 0; i < repeatCount; ++i) {
-            std::cout << "开始发送消息第 " << i + 1 << " 次" << std::endl;
+        if (!message.empty()) {
+            // 模式 1：发送自定义消息
+            for (int i = 0; i < repeatCount; ++i) {
+                std::cout << "发送第 " << i + 1 << " 次: " << message << std::endl;
 
-            for (char ch : message) {
-                sendChar(ch);
-                std::this_thread::sleep_for(std::chrono::milliseconds(intervalBetweenCharsMs));
+                for (char ch : message) {
+                    sendChar(ch);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(intervalBetweenCharsMs));
+                }
+
+                sendEnter();
+                std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(intervalBetweenMessagesMs));
             }
+        }
+        else {
+            // 模式 2：发送从1到repeatCount的数字
+            for (int i = 1; i <= repeatCount; ++i) {
+                std::string numStr = std::to_string(i);
+                std::cout << "发送数字：" << numStr << std::endl;
 
-            sendEnter();
-            std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(intervalBetweenMessagesMs));
+                for (char ch : numStr) {
+                    sendChar(ch);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(intervalBetweenCharsMs));
+                }
+
+                sendEnter();
+                std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(intervalBetweenMessagesMs));
+            }
         }
 
         std::cout << "消息发送完毕喵~" << std::endl;
--- InputHandler.cpp
+++ InputHandler.cpp
@@ -4,13 +4,14 @@
 
 void getUserInput(std::string& message, int& count ,double& interval)
 {
-    std::cout << "请输入消息内容：";
-    std::getline(std::cin, message);
+	int Method = 0;
+	std::cout << "(1)使用消息+数量 (2)数量" << std::endl;
 
+    // 判断合法性
     while (true)
     {
-        std::cout << "请输入消息数量：";
-        if (!(std::cin >> count) || count < 0)
+        std::cout << "请输入 1 或者 2 选择发送方式：";
+        if (!(std::cin >> Method) || Method <= 0 || Method >= 3)
         {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -23,6 +24,47 @@ void getUserInput(std::string& message, int& count ,double& interval)
         }
     }
 
+    if (Method == 1)
+    {
+        std::cout << "请输入消息内容：";
+        std::getline(std::cin, message);
+
+        while (true)
+        {
+            std::cout << "请输入消息数量：";
+            if (!(std::cin >> count) || count < 0)
+            {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "输入无效，请重新输入。" << std::endl;
+            }
+            else
+            {
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                break;
+            }
+        }
+    }
+    else
+    {
+        while (true)
+        {
+            std::cout << "请输入消息数量：";
+            if (!(std::cin >> count) || count < 0)
+            {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "输入无效，请重新输入。" << std::endl;
+            }
+            else
+            {
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                break;
+			}
+        }
+    }
+
+    // 询问键入间隔
     while (true)
     {
         std::cout << "请输入消息间隔（毫秒）：";
@@ -35,7 +77,6 @@ void getUserInput(std::string& message, int& count ,double& interval)
         else
         {
             break;
-		}
+        }
     }
-
 }
